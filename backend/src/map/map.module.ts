e2c5b923a004056acad5642903a7fb10ada332b9
celalkale-gitcode <celalkale@gmail.com>
2026-05-02 00:21:31 +0300

    Add MapModule with MapGateway and PrismaService

--- backend/src/map/map.module.ts
+++ backend/src/map/map.module.ts
@@ -1,1 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MapGateway } from './map.gateway';
+import { PrismaService } from '../common/prisma.service';
+
+@Module({
+  providers: [MapGateway, PrismaService],
+})
+export class MapModule {}
 
