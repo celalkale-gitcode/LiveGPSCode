b7f13c44ddcc3803ddd2d39f69b130bc19d1d97f
celalkale-gitcode <celalkale@gmail.com>
2026-05-02 00:22:25 +0300

    Enable CORS and log application port
    Enable CORS for the application and add a console log for the port.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Frontend'den gelen istekleri engellememesi için
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Uygulama port ${process.env.PORT || 3000} üzerinde çalışıyor`);
+}
+bootstrap();
 
